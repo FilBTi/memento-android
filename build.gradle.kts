dea6854183ae78e60da3a1839f089505f6702053
Puroblast <Puroblast97@mail.ru>
2024-01-09 20:27:34 +0300

    detekt setup + java version change

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,42 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
+
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.detekt)
 }
-true // Needed to make the Suppress annotation work for the plugins block
+
+
+allprojects {
+
+    apply(plugin = rootProject.project.libs.plugins.detekt.get().pluginId)
+    apply(plugin = "base")
+
+    dependencies {
+        detektPlugins(rootProject.project.libs.util.detektPlugin)
+        detektPlugins(rootProject.project.libs.util.detektFormatting)
+    }
+
+    tasks.withType<Detekt>().configureEach {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+        setSource(files("src"))
+        exclude("**/androidTest/**")
+        exclude("**/assets/**")
+        exclude("**/res/**")
+        exclude("**/test/**")
+        exclude("**/test-ui-*/**")
+
+    }
+    tasks.withType<DetektCreateBaselineTask>().configureEach {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+    detekt {
+        toolVersion = rootProject.project.libs.versions.detekt.get()
+        buildUponDefaultConfig = true
+        autoCorrect = true
+    }
+}
+tasks.register("projectClean", Delete::class) {
+    delete(setOf(rootProject.buildDir))
+}
