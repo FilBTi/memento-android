6704e2c233ed6318e74d0d41cf4a162d7f6efcca
Puroblast <Puroblast97@mail.ru>
2024-01-12 19:27:31 +0300

    home fragment initial setup + navigation to it

--- feature-auth/src/main/java/com/puroblast/feature_auth/AuthFragment.kt
+++ feature-auth/src/main/java/com/puroblast/feature_auth/AuthFragment.kt
@@ -2,7 +2,10 @@ package com.puroblast.feature_auth
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavDeepLinkRequest
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.puroblast.feature_auth.databinding.FragmentAuthBinding
 
@@ -23,6 +26,13 @@ class AuthFragment : Fragment(R.layout.fragment_auth) {
                 binding.linkTextView.setText(R.string.register_underlined_text)
                 binding.accountTextView.setText(R.string.dont_you_have_account_text)
             }
+        }
+
+        binding.joinButton.setOnClickListener {
+            val request = NavDeepLinkRequest.Builder
+                .fromUri("android-app://com.puroblast.feature_folders/homeFragment".toUri())
+                .build()
+            findNavController().navigate(request)
         }
     }
 }
---
+++ feature-folders/src/main/java/com/puroblast/feature_folders/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.puroblast.feature_folders
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+}
